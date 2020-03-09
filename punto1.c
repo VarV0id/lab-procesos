ca3cb90c9643302d1d5853248a57e7bfa65928fa
varv0id <jairocampana10001@gmail.com>
2020-03-09 02:24:04 -0500

    Codigo Punto 1

---
+++ punto1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    int x = 100;
+    if(fork() == 0){
+        printf("x %d en el proceso hijo %d\n", x, getpid());
+        x = 20;
+        printf("x actualizado %d en el proceso hijo %d\n", x, getpid());
+    }else{
+        printf("x %d en el proceso padre %d\n", x, getpid());
+        x = 30;
+        printf("x actualizado %d en el proceso padre %d\n", x, getpid());
+    }
+}
