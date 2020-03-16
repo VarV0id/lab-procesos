e68487d991e2a8df154f0bf3ec83bded25f62ca5
Sebas Pelaez <sebaspvilla@hotmail.com>
2020-03-15 23:02:24 -0500

    Punto 3 Listo

---
+++ punto3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+    if(fork() == 0){
+        printf("Hello!!);
+    }else{
+        sleep(1);
+        printf("Good Bye!!);
+    }
+    return 0;
+}
